9ab144e0491a7becf333bb885c88624a5567bc88
addonion <addonion@gmail.com>
2023-10-20 20:45:44 +0500

    Project page

--- app/portfolio-intereri/[slug]/page.tsx
+++ app/portfolio-intereri/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import { ProjectPageProps } from "../../@types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Project(props: ProjectPageProps) {
   const { data } = await getData(props);
-  console.log(data.attributes.content);
+  const media = data.attributes.content;
 
   return (
     <div className="container mx-auto">
@@ -10,10 +12,32 @@ export default async function Project(props: ProjectPageProps) {
         <h1>Проект: «{data.attributes.title}»</h1>
       </div>
 
-      <div className="columns-2 md:columns-3 xl:columns-4"></div>
+      {media.map((block: any) => {
+        if (block.__component === "media.gallery") return <Gallery data={block.gallery.data} />;
+        if (block.__component === "media.3-d-tour") return <Tour3D data={block.ftp} />;
+      })}
     </div>
   );
 }
+
+const Tour3D = ({ data }: { data: any }) => {
+  return <iframe id="inlineFrameExample" title="Inline Frame Example" className="pb-24 w-full h-screen" src={data} />;
+};
+
+const Gallery = ({ data }: { data: any }) => {
+  const gallery = data.map((item: any) => item.attributes);
+  return (
+    <div className="columns-2 md:columns-3 xl:columns-4 pb-24">
+      {gallery.map((pic: any) => {
+        return (
+          <div key={pic.hash}>
+            <Image src={pic.formats.medium.url} width={pic.formats.medium.width} height={pic.formats.medium.height} alt="Изображение" blurDataURL={pic.placeholder} placeholder="blur" className="mb-4" />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 async function getData(props: ProjectPageProps) {
   const { data } = await getPostId(props.params.slug);
