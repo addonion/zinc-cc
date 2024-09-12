4c56f9abb5f7505110184c8e67968c4fd7bd7124
addonion <addonion@gmail.com>
2024-09-12 16:54:49 +0500

    Add Contacts

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -9,19 +9,43 @@ export default async function Portfolio() {
   const { data } = await getData();
 
   return (
-    <div className="container mx-auto">
-      <div className="pagetitle text-center text-white py-24">
-        <h1>Контактная информация</h1>
-        <div>
-          <b>контакты, телефоны, почта и адрес</b>
+    <>
+      <div className="container mx-auto">
+        <div className="pagetitle text-center text-white py-24">
+          <h1>Контактная информация</h1>
+          <div>
+            <b>контакты, телефоны, почта и адрес</b>
+          </div>
         </div>
       </div>
-    </div>
+
+      <article className="line_box">
+        <div className="container mx-auto">
+          <div className="flex gap-24 py-6">
+            {data.map((team: any) => {
+              const { Name, Role, Phone, Email } = team.attributes
+              return (
+                <div key={team.id} className="w-1/3 mx-auto">
+                  <h2>{Name}</h2>
+                  <p>{Role}</p>
+                  <p>
+                    Телефон: <a href={`tel:+${Phone.replace(/[+-\s]/g, "")}`}>{Phone}</a>
+                  </p>
+                  <p>
+                    Email: <a href={`mailto:${Email}`}>{Email}</a>
+                  </p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </article>
+    </>
   );
 }
 
 async function getData() {
-  const res = await fetch(`${process.env.SERVER_HOST}/api/projects?populate[content][populate]=gallery&populate=picture`);
+  const res = await fetch(`${process.env.SERVER_HOST}/api/teams`);
 
   // Recommendation: handle errors
   if (!res.ok) {
